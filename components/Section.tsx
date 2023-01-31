d73e86ed949a6436f4dd61e684e598450dab2610
Jordane Gengo <jordane@hive.fi>
2023-01-31 13:32:44 +0200

    design: add a hover effect on sections

--- components/Section.tsx
+++ components/Section.tsx
@@ -5,7 +5,7 @@ import { SectionProps } from './types'
 
 export default function Section({ title, outstanding, completed, delayed }: SectionProps) {
   return (
-    <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-3 sm:before:h-3 sm:before:rounded-full sm:before:left-[-33px] sm:before:z-[1] before:border-2 before:border-mypurple before:bg-mycardbg hover:before:bg-mypurple">
+    <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-3 sm:before:h-3 sm:before:rounded-full sm:before:left-[-33px] sm:before:z-[1] before:border-2 before:border-mypurple before:bg-mycardbg hover:before:bg-mypurple sm:hover:before:w-6 before:transition before:duration-300">
       <h3 className="text-lg font-semibold tracking-wide text-mypurple sm:before:border-t-2 before:w-12  before:absolute before:top-[13px] before:left-[-33px] before:border-solid before:border-mypurple sm:ml-6 uppercase font-mono">
         {title}
       </h3>
