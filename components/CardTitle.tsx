bc281765974ad0152f980c09376575a3e09fa00d
Jordane Gengo <jordane@hive.fi>
2023-02-03 10:06:03 +0200

    fix: remove useless and broken border

--- components/CardTitle.tsx
+++ components/CardTitle.tsx
@@ -2,7 +2,7 @@ import { CardTitleProps } from './types'
 
 export default function CardTitle({ title, completedTasks }: CardTitleProps) {
   return (
-    <div className="mt-8 px-5 text-xl font-bold w-full bg-[#fd0]/70 border-t-[1px] border-b-[1px] border-[#ffffff14] p-2 h-15 flex justify-between items-center">
+    <div className="mt-8 px-5 text-xl font-bold w-full bg-[#fd0]/70 p-2 h-15 flex justify-between items-center">
       <p className="tracking-wide capitalize">{title}</p>
       <div className="text-xs font-normal">tasks completed: {completedTasks}</div>
     </div>
